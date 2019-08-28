72db97d902a44f50d288359ba60d542a96542e53
Harsh Gupta <harsh.gupta@amantechnologiesinc.com>
2019-08-28 06:05:45 +0530

    mean code structure

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-lists',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent {}
